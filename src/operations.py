94885968bed98cd09f46a2854406205f5ac5bc6a
daarani24 <daarani2007@gmail.com>
2026-07-30 22:03:44 +0530

    Added delete and update features

--- src/operations.py
+++ src/operations.py
@@ -17,3 +17,30 @@ def view_students(students):
         print("Name:",s[1])
         print("Department:",s[2])
         print("Year:",s[3])
+
+def update_student(students):
+    roll_no=int(input("Enter Roll No:"))
+    for s in students:
+        if s[0]==roll_no:
+            print("student Found")
+            s[1]=input("Enter new name:")
+            s[2]=input("Enter new department:")
+            s[3]=input("Enter new year:")
+            print("Student updated successfully")
+            break
+    else:
+        print("Student not found")
+
+def delete_student(students):
+    if len(students)==0:
+        print("No student detail is found")
+        return
+    roll_no=input("Enter Roll No:")
+    for s in students:
+        if s[0]==roll_no:
+            students.remove(s)
+            print("Student deleted successfully")
+            break
+    else:
+        print("Student not found")
+
