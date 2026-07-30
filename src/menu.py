5ddf152cfa6ba909e80050f9ccf11e4f4819fb17
daarani24 <daarani2007@gmail.com>
2026-07-30 22:02:52 +0530

    updated menu.py

--- src/menu.py
+++ src/menu.py
@@ -2,6 +2,9 @@ def menu():
     print("Student Progress Tracker")
     print("1.Add Student")
     print("2.View student")
-    print("3.Exit")
+    print("3.Update student")
+    print("4.Delete student")
+    print("5.Search student")
+    print("6.Exit")
 
     return input("Enter your choice")
